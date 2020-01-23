96c5b209671cd72c2fdb4602bc27114d8cad1e92
iaia <iaia72160@gmail.com>
2020-01-23 21:15:25 +0900

    Add libraries for instrumentaion test

--- app/src/androidTest/java/com/example/daggerandroidtest/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/daggerandroidtest/MainActivityTest.kt
@@ -4,11 +4,21 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
+@LargeTest
 class MainActivityTest {
+
+    @get:Rule
+    var activityScenarioRule = activityScenarioRule<MainActivity>()
 
     @Before
     fun setUp() {
